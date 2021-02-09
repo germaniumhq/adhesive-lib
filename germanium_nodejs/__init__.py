06c6f32c2e0a234844f9a4ee03eeccf71416c8a8
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2021-02-09 11:28:30 +0100

    Added support for nodejs applications

---
+++ germanium_nodejs/__init__.py
@@ -0,0 +1,17 @@
+import os
+from typing import Optional
+
+import adhesive
+
+from germanium_nodejs.pipeline_types import PipelineConfig
+from germanium_nodejs.steps import noop
+
+noop()
+
+
+def pipeline(config: Optional[PipelineConfig] = None) -> None:
+    script_dir = os.path.dirname(__file__)
+
+    adhesive.bpmn_build(
+        os.path.join(script_dir, "build_node.bpmn"),
+        initial_data={})
